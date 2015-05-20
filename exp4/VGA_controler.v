e5bad3e08698a33e97b4e23666b87d9c36476f02
gmo cornejo <gmocornejos@gmail.com>
2015-05-20 07:52:20 -0600

    anadi el controlador VGA, version 1.0 y una declaracion de pines para poder flashear la FPGA sin la miniALU

--- exp4/VGA_controler.v
+++ exp4/VGA_controler.v
@@ -2,20 +2,29 @@
 `timescale 1ns/100ps
 
 module vga_controller(
-	input  wire       clock,
+	input  wire       clock_ref, // 50 MHz
 	input  wire       reset,
-	input  wire [2:0] rgb, 
+	input  wire [2:0] iRGB, 
 	output wire       h_sync,
 	output wire       v_sync,
 	output wire [8:0] row,
 	output wire [9:0] column,
-	output reg  [2:0] o_rgb
+	output reg  [2:0] oRGB
 );
 
 wire reset_h_counter, reset_v_counter, reset_row_counter;
 wire enable_row_counter;
 wire [9:0] h_counter;
 wire [18:0] v_counter;
+wire clock;  // 25 MHz
+
+UPCOUNTER_POSEDGE #(1) tmp_counter(
+	.Clock(clock_ref),
+	.Reset(reset),
+	.Initial(1'b1),
+	.Enable(1'b1),
+	.Q(clock)
+);
 
 UPCOUNTER_POSEDGE #(10) horizontal_counter(
 	.Clock(clock), 
@@ -51,6 +60,6 @@ assign h_sync = (h_counter < 10'd95) ? 0 : 1;
 assign v_sync = (v_counter < 19'd1599) ? 0 : 1;
 
 always @ (posedge clock)
-	o_rgb <= rgb;
+	oRGB <= 3'b010;
 
 endmodule

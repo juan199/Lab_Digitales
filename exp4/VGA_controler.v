f158e68a9350db1ea1bcb71fe85c0e541f76f486
gmo cornejo <gmocornejos@gmail.com>
2015-05-24 21:41:35 -0600

    vga_controller.v modificado según inspiración dada por los estudiantes del semestre pasado. Se actualizó la declaración de pines en vga_pin.ucf

--- exp4/VGA_controler.v
+++ exp4/VGA_controler.v
@@ -12,13 +12,13 @@ module vga_controller(
 	output reg  [2:0] oRGB
 );
 
-wire reset_h_counter, reset_v_counter, reset_row_counter;
-wire enable_row_counter;
+wire reset_h_counter, reset_v_counter;
+wire enable_v_counter;
 wire [9:0] h_counter;
-wire [18:0] v_counter;
+wire [9:0] v_counter;
 wire clock;  // 25 MHz
 
-UPCOUNTER_POSEDGE #(1) tmp_counter(
+UPCOUNTER_POSEDGE #(1) clock_divisor(
 	.Clock(clock_ref),
 	.Reset(reset),
 	.Initial(1'b1),
@@ -34,30 +34,21 @@ UPCOUNTER_POSEDGE #(10) horizontal_counter(
 	.Q(h_counter)
 );
 
-UPCOUNTER_POSEDGE #(19) vertical_counter(
+UPCOUNTER_POSEDGE #(10) vertical_counter(
 	.Clock(clock), 
 	.Reset(reset_v_counter),
-	.Initial(19'b0),
-	.Enable(1'b1),
+	.Initial(10'b0),
+	.Enable(enable_v_counter),
 	.Q(v_counter)
 );
 
-UPCOUNTER_POSEDGE #(9) row_counter(
-	.Clock(clock), 
-	.Reset(reset_row_counter),
-	.Initial(9'b0),
-	.Enable(enable_row_counter),
-	.Q(row)
-);
-
-assign reset_h_counter = reset | (h_counter == 10'd799) ? 1 : 0;
-assign reset_v_counter = reset | (v_counter == 19'd416799) ? 1 : 0; 
-assign reset_row_counter = reset | (row == 9'd479) ? 1 : 0 | (v_counter == 19'd9599);
-// Si la figura de la pantalla se mueve raro, intente quitando (v_counter == 19'd9599)
-assign enable_row_counter = reset_h_counter;
-assign column = (h_counter > 10'd143 && h_counter < 10'd623) ? (h_counter - 10'd143) : 0;
-assign h_sync = (h_counter < 10'd95) ? 0 : 1;
-assign v_sync = (v_counter < 19'd1599) ? 0 : 1;
+assign reset_h_counter = reset | (h_counter == 10'd799);
+assign reset_v_counter = reset | (v_counter == 10'd521);  // 520 ó 521??
+assign enable_v_counter = reset_h_counter;
+assign h_sync = h_counter < 10'd96 ? 1'b0 : 1'b1;
+assign v_sync = v_counter < 10'd2 ? 1'b0 : 1'b1;
+assign row = (v_counter > 10'd11 & v_counter < 10'd491) ? (v_counter - 10'd11) : 10'b0 ;
+assign column = (h_counter > 10'd143 & h_counter < 10'd783) ? (h_counter - 10'd143) : 10'b0 ; 
 
 always @ (posedge clock)
 	oRGB <= 3'b010;

68031bc0b35107f72ad03205ccaafb033412e972
juan199 <juansanchezbrenes@gmail.com>
2015-07-12 20:25:06 -0600

    Controlador microprogramado

--- exp4/Collaterals.v
+++ exp4/Collaterals.v
@@ -69,4 +69,23 @@ endcase
 
 end
 endmodule
+//*********************************************************************
+//RAM Controller
+//*********************************************************************
+module RAM_controller(
+	inout [15:0], // Input/Output port
+	output  reg address [17:0], // Address
+	output reg CE, //Chip Select Enaeble (en cero)
+   output UB, //High Byte [16-bit data word] (en cero)
+	output LB,// Low byte [16-bit data word] (en cero)
+	output WE, //write-enable (en cero)
+	output OE //read-enable (en cero)
+	
+	
+	
 
+
+);
+
+
+endmodule
